230c4e61efdd22526506e1b22ad891650ac2a050
robbryandev <robbryandev@gmail.com>
2022-11-14 08:32:09 -0800

    Add Age earthAge test

--- __tests__/age.test.ts
+++ __tests__/age.test.ts
@@ -10,4 +10,8 @@ describe("Age class", () => {
   test("Should return default earth year length", () => {
     expect(testAge.yearLength).toEqual(365);
   });
+
+  test("Should return default earth age of 1", () => {
+    expect(testAge.earthAge).toEqual(1);
+  });
 });
--- src/scripts/age.ts
+++ src/scripts/age.ts
@@ -1,5 +1,6 @@
 export default class Age {
   yearLength: number;
+  earthAge: number;
   constructor(yearLength = 365) {
     this.yearLength = yearLength;
   }
