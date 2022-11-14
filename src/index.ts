a17cfeb932c14c4cf65e55986a7efa9aa6930c16
robbryandev <robbryandev@gmail.com>
2022-11-14 11:30:10 -0800

    Add results to ui

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import "bootstrap";
 import * as $ from "jquery";
+import Age from "./scripts/age";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/css/styles.css";
 
 function handleForm() {
-  console.log(0);
+  const yearDays = Number($("#days").val());
+  const ageNum = Number($("#age").val());
+  const spaceAge = new Age(yearDays, ageNum);
+  $("#result").html(`
+    <h1>Result:</h1>
+    <h3>Age: ${ageNum}</h3>
+    <h3>Days in year: ${yearDays}</h3>
+    <h3>Space Age: ${spaceAge.getAge()}</h3>
+    <h3>Years left: ${spaceAge.personalExpect()}</h3>
+  `);
 }
 
 $("form").on("submit", (event) => {
