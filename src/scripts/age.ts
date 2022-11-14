0926efca3fc6a6774fdad20f6dcf3478eecdb40f
robbryandev <robbryandev@gmail.com>
2022-11-14 09:15:28 -0800

    Add venusAge method to Age class

--- src/scripts/age.ts
+++ src/scripts/age.ts
@@ -1,24 +1,24 @@
 export default class Age {
   yearLength: number;
   earthAge: number;
+  readonly earthYear = 365;
   constructor(yearLength = 365, earthAge = 1) {
     this.yearLength = yearLength;
     this.earthAge = earthAge;
   }
 
   getAge(yearVar: number = this.yearLength) {
-    const earthYear = 365;
-    const yearDec = yearVar / earthYear;
+    const yearDec = yearVar / this.earthYear;
     return Math.floor(this.earthAge / yearDec);
   }
 
   mercuryAge() {
-    const earthYear = 365;
-    const mercYear = earthYear * 0.24;
+    const mercYear = this.earthYear * 0.24;
     return this.getAge(mercYear);
   }
 
   venusAge() {
-    return 0;
+    const venYear = this.earthYear * 0.62;
+    return this.getAge(venYear);
   }
 }
