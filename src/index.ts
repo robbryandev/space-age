66375b4722edcf6c33d82918714185bdf132d005
robbryandev <robbryandev@gmail.com>
2022-11-14 11:13:14 -0800

    Add handleForm function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import "bootstrap";
+import * as $ from "jquery";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/css/styles.css";
+
+function handleForm() {
+  console.log(0);
+}
+
+$("form").on("submit", (event) => {
+  event.preventDefault();
+  handleForm();
+});
